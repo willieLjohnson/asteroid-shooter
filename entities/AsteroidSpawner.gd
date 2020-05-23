c74b99b05472c1a2be6c699a383a40939b53483f
willieLjohnson <liwa.johnson@gmail.com>
2020-05-23 14:17:52 -0400

    Add randomized asteroid spawning

--- entities/AsteroidSpawner.gd
+++ entities/AsteroidSpawner.gd
@@ -8,6 +8,13 @@ func _ready() -> void:
 func _spawn_asteroid() -> void:
 	var asteroid = asteroid_scene.instance()
 	
-	asteroid.position = Vector2(50, 0)
+	_set_asteroid_position(asteroid)
 	
 	add_child(asteroid)
+
+func _set_asteroid_position(asteroid) -> void:
+	var rect = get_viewport().size
+	asteroid.position = Vector2(rand_range(0, rect.x), -100)
+
+func _on_SpawnTimer_timeout() -> void:
+	_spawn_asteroid()
