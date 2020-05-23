847328a38da8cf62f33578df7b604edd6118521e
willieLjohnson <liwa.johnson@gmail.com>
2020-05-23 15:26:05 -0400

    Randomize small asteroid trajectory

--- objects/Asteroid.gd
+++ objects/Asteroid.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
 
 var asteroid_small_scene := load("res://objects/AsteroidSmall.tscn")
+var rand_num_generator := RandomNumberGenerator.new()
+
 var is_exploded := false
 
 func explode() -> void:
@@ -9,15 +11,31 @@ func explode() -> void:
 		
 	is_exploded = true
 	
-	_spawn_asteroid_small()
+	_spawn_asteroid_smalls(4) 
 	
 	get_parent().remove_child(self)
 	queue_free()
 
+func _spawn_asteroid_smalls(num: int) -> void:
+	for i in range (num):
+		_spawn_asteroid_small()
+	
 func _spawn_asteroid_small() -> void:
 	var asteroid_small = asteroid_small_scene.instance()
 	asteroid_small.position = self.position
+	_randomize_trajectory(asteroid_small)
 	get_parent().add_child(asteroid_small)
+	
+func _randomize_trajectory(asteroid) -> void:
+	asteroid.angular_velocity = rand_range(-4, 4)
+	asteroid.angular_damp = 0
+	
+	rand_num_generator.randomize()
+	var linear_vel_x = rand_num_generator.randi_range(-1, 1)
+	var linear_vel_y = rand_num_generator.randi_range(-1, 1)
+	
+	asteroid.linear_velocity = Vector2(linear_vel_x * 400, linear_vel_y * 400)
+	asteroid.linear_damp = 0
 
 func _on_VisibilityNotifier2D_viewport_exited(viewport: Viewport) -> void:
 	queue_free()
