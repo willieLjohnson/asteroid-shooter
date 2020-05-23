9655224a2153395c1249182176a16978ed76a336
willieLjohnson <liwa.johnson@gmail.com>
2020-05-23 13:56:02 -0400

    Optimize laser script

--- objects/Laser.gd
+++ objects/Laser.gd
@@ -5,3 +5,7 @@ var projectile_speed := 1000
 
 func _process(delta: float) -> void:
 	self.position += direction * projectile_speed * delta
+
+
+func _on_VisibilityNotifier2D_viewport_exited(viewport):
+	queue_free()
