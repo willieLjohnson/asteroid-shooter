1224cd6fcd885caee618c352a302aee3348f81e0
willieLjohnson <liwa.johnson@gmail.com>
2020-05-23 14:52:33 -0400

    Spawn smaller asteroid upon destroying big one

--- objects/Asteroid.gd
+++ objects/Asteroid.gd
@@ -1,16 +1,23 @@
 extends RigidBody2D
 
+var asteroid_small_scene := load("res://objects/AsteroidSmall.tscn")
 var is_exploded := false
 
-func explode():
+func explode() -> void:
 	if is_exploded:
 		return
 		
 	is_exploded = true
 	
+	_spawn_asteroid_small()
+	
 	get_parent().remove_child(self)
 	queue_free()
 
+func _spawn_asteroid_small() -> void:
+	var asteroid_small = asteroid_small_scene.instance()
+	asteroid_small.position = self.position
+	get_parent().add_child(asteroid_small)
 
 func _on_VisibilityNotifier2D_viewport_exited(viewport: Viewport) -> void:
 	queue_free()
