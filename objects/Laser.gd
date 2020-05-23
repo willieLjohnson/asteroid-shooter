3f842e2a20b732f89976ab35fa71be9d11c13b7c
willieLjohnson <liwa.johnson@gmail.com>
2020-05-23 14:06:08 -0400

    Destroy asteroid upon collision with laser

--- objects/Laser.gd
+++ objects/Laser.gd
@@ -7,5 +7,14 @@ func _process(delta: float) -> void:
 	self.position += direction * projectile_speed * delta
 
 
-func _on_VisibilityNotifier2D_viewport_exited(viewport):
+func _on_VisibilityNotifier2D_viewport_exited(viewport: Viewport) -> void:
 	queue_free()
+ 
+
+
+func _on_Laser_body_shape_entered(body_id: int, body: Node, body_shape: int, area_shape: int) -> void:
+		if (body.is_in_group("asteroids")):
+			body.call_deferred("explode")
+			get_parent().remove_child(self)
+			queue_free()
+			
